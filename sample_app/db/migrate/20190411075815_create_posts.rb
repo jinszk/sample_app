a8df6453d4572617b87713713b2906e1e95b4056
jinszk <jinszk1991@gmail.com>
2019-04-12 17:29:23 +0900

    [Update]3-5

--- sample_app/db/migrate/20190411075815_create_posts.rb
+++ sample_app/db/migrate/20190411075815_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
 
-        t.string :titile
+        t.string :title
         t.string :body
         t.timestamps
     end
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_075815) do
+ActiveRecord::Schema.define(version: 2019_04_12_064409) do
 
   create_table "posts", force: :cascade do |t|
     t.string "titile"
